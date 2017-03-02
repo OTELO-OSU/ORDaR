a422380c71e6ed16c5a5254a52a6e3ca29055a39
Anthony Guiot <anthony.guiot@univ-lorraine.fr>
2017-03-02 09:44:08 +0100

    Script Verification Embargoed access ok

--- Check_Embargoed_access.php
+++ Check_Embargoed_access.php
@@ -1,3 +1,16 @@
 <?php
 
+$db = new MongoClient("mongodb://localhost:27017");
+$collection= $db->selectCollection('ORDaR', 'Manual_Depot');
+$query=array('INTRO.ACCESS_RIGHT' => 'Embargoed');
+$cursor = $collection->find($query);
+foreach ( $cursor as $id => $value )
+{
+    $now= Date('Y-m-d');
+    $embargoeddate=$value['INTRO']['PUBLICATION_DATE'];
+    if($embargoeddate <= $now) {
+    	$update = $collection->update(array("_id" => $value['_id']), array('$set' => array("INTRO.ACCESS_RIGHT" => "Open")));
 
+    }
+
+}
