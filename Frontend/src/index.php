fd6154b7e7291b9b198a08beeafdea476cfb26da
Anthony Guiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-06-20 15:30:17 +0200

    Fix login

--- Frontend/src/index.php
+++ Frontend/src/index.php
@@ -109,9 +109,9 @@ $app->get('/login', function (Request $req, Response $responseSlim) {
     echo $twig->render('login.html.twig');
     $config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/../config.ini');
   
-   $_SESSION['name'] = "t";
-   $_SESSION['firstname'] = "t";
-   $_SESSION['mail'] = "t@t.fr";
+   $_SESSION['name'] = $_SERVER['HTTP_SN'];
+   $_SESSION['firstname'] = $_SERVER['HTTP_GIVENNAME'];
+   $_SESSION['mail'] = $_SERVER['HTTP_MAIL'];
       
 
     
