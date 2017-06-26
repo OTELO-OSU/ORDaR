744ea491f17b142e4aa5021707d6aa25e34b9a45
Anthony Guiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-06-26 14:25:12 +0200

    fix bug

--- Frontend/src/js/search.js
+++ Frontend/src/js/search.js
@@ -282,8 +282,6 @@
                       }
                   });
                   $('#facets').show();
-                   $( window ).resize(function() {
-location.reload()});
                    if (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
       $('.facets').hide();
       $('form .ui.grid#gridresults').prepend('<div id="Displayfacets" class="ui button primary">Display/Hide facets</div>');
@@ -612,8 +610,6 @@ location.reload()});
                       }
                   });
                   $('#facets').show();
-                   $( window ).resize(function() {
-                    location.reload()});
               if (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
       $('.facets').hide();
       $('form .ui.grid').prepend('<div id="Displayfacets" class="ui button primary">Display/Hide facets</div>');
