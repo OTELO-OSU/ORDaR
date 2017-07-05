3c1c197229a7852cf661f09ead4f1002c61e15bc
Anthony Guiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-07-05 14:06:21 +0200

    Modification bug responsive

--- Frontend/src/js/search.js
+++ Frontend/src/js/search.js
@@ -282,14 +282,17 @@
                       }
                   });
                   $('#facets').show();
-                   if (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
+                   if ($(window).width()<=1046) {
       $('.facets').hide();
       $('form .ui.grid#gridresults').prepend('<div id="Displayfacets" class="ui button primary">Display/Hide facets</div>');
       $('form .ui.grid .button').on("click",function(){
          $(".facets").toggle();
       })
-      $('#accueil').removeClass();
     }
+      if ($(window).width()<=420) {
+        $('#accueil').removeClass();
+
+       }
               }
           },
           //Methode de trap des events sur les facets
@@ -610,14 +613,17 @@
                       }
                   });
                   $('#facets').show();
-              if (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
+      if ($(window).width()<=1046) {
       $('.facets').hide();
       $('form .ui.grid').prepend('<div id="Displayfacets" class="ui button primary">Display/Hide facets</div>');
       $('form .ui.grid .button').on("click",function(){
          $(".facets").toggle();
       })
-        $('#accueil').removeClass();
       } 
+       if ($(window).width()<=420) {
+        $('#accueil').removeClass();
+
+       }
 
               }
           },
