4e8729ce338e8600c270225176a27d4cebbd1816
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-09-19 16:20:58 +0200

    Bug fix timeout Mail docker

--- Frontend/src/search/controller/MailerController.php
+++ Frontend/src/search/controller/MailerController.php
@@ -6,6 +6,22 @@ use \search\controller\FileController as File;
 Class MailerController
 {
 
+
+	function CheckSMTPstatus(){
+		 	$file = new File();
+	        $config=$file->ConfigFile();
+	  		$f = fsockopen($config['SMTP'], 25,$errno,$errstr,3) ;
+	  		$connected=false;
+				if ($f !== false) {
+				    $res = fread($f, 1024) ;
+					if (strlen($res) > 0 && strpos($res, '220') === 0) {
+					    $connected=true; 
+					}
+				}
+			fclose($f) ;
+			return $connected;
+	}
+
     /**
      * Send a mail to author of a dataset
      * @param  doi of dataset , data of dataset,nom de l'auteur,prenom de l'auteur,object du mail,message, mail de l'expediteur
@@ -14,6 +30,8 @@ Class MailerController
     
     function Send_Mail_author($doi, $response, $author_name, $author_firstname, $object, $message, $sendermail)
     {
+    	$connected=self::CheckSMTPstatus();
+	if ($connected===true) {
             $file = new File();
     $config=$file->ConfigFile();
 
@@ -63,6 +81,10 @@ Class MailerController
         } else {
             return $error = "true";
         }
+    }
+    else {
+            return $error = "true";
+        }
         
     }
         /**
@@ -72,31 +94,39 @@ Class MailerController
      */
     function Send_Contact_Mail($object, $message, $sendermail)
     {
-        $file = new File();
-        $config=$file->ConfigFile();
-        if (!empty($object) && !empty($message) && filter_var($sendermail, FILTER_VALIDATE_EMAIL)) {
-            $headers = "From:<".$config['NO_REPLY_MAIL'].">\r\n";
-            $headers .= "MIME-Version: 1.0\r\n";
-            $headers .= "Content-Type: text/html; charset=utf-8\r\n";
-            $mail = mail("<otelo-si@univ-lorraine.fr>", 'Contact from '.$config['REPOSITORY_NAME'].': ' . $object, '<html>
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    </head>
-    <body>
-         <h2>Contact from :  <img src="'.$config['REPOSITORY_URL'].'/img/logo.png" alt="Logo" height="30" width="120" /> </h2>  
-        <table cellspacing="0" style="border: 2px solid black; width: 400px; height: 200px;">
-            <tr>
-                <th>From:</th><td>' . $sendermail . '</td>
-            </tr>
-            <tr style="background-color: #e0e0e0;">
-                <th>Subject:</th><td>' . $object . '</td>
-            </tr>
-            <tr>
-                <th valign="bottom">Message:</th><td>' . $message . '</td>
-            </tr>
-        </table>
-    </body>
-    </html> ', $headers);
+  	$connected=self::CheckSMTPstatus();
+	if ($connected===true) {
+	
+
+		        $file = new File();
+		        $config=$file->ConfigFile();
+		        if (!empty($object) && !empty($message) && filter_var($sendermail, FILTER_VALIDATE_EMAIL)) {
+		            $headers = "From:<".$config['NO_REPLY_MAIL'].">\r\n";
+		            $headers .= "MIME-Version: 1.0\r\n";
+		            $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+		            $mail = mail("<otelo-si@univ-lorraine.fr>", 'Contact from '.$config['REPOSITORY_NAME'].': ' . $object, '<html>
+		    <head>
+		    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		    </head>
+		    <body>
+		         <h2>Contact from :  <img src="'.$config['REPOSITORY_URL'].'/img/logo.png" alt="Logo" height="30" width="120" /> </h2>  
+		        <table cellspacing="0" style="border: 2px solid black; width: 400px; height: 200px;">
+		            <tr>
+		                <th>From:</th><td>' . $sendermail . '</td>
+		            </tr>
+		            <tr style="background-color: #e0e0e0;">
+		                <th>Subject:</th><td>' . $object . '</td>
+		            </tr>
+		            <tr>
+		                <th valign="bottom">Message:</th><td>' . $message . '</td>
+		            </tr>
+		        </table>
+		    </body>
+		    </html> ', $headers);
+    }else {
+            return $error = "true";
+        }
+
             if ($mail == true) {
                 $error = "false";
             } else {
@@ -112,42 +142,49 @@ Class MailerController
      * @return true if error, else false
      */
     function Send_Mail_To_uploader($authors,$title, $doi, $description)
-    {        
-        $file = new File();
-        $config=$file->ConfigFile();
-        $headers = "From:<".$config['NO_REPLY_MAIL'].">\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
-        foreach ($authors as $key => $value) {
-        $mail = mail($value['MAIL'], 'Dataset submit successfully! ', '<html>
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    </head>
-    <body>
-        <h2>Your dataset is now on <img src="'.$config['REPOSITORY_URL'].'/img/logo.png" alt="Logo " height="30" width="120" /></h2>
-        <p> '.$_SESSION['name'].' '.$_SESSION['firstname'].' has published this dataset </p>
-        <p> Your DOI is : <a href="http://dx.doi.org/' . $doi . '">' . $doi . '</a></p>
-         <table cellspacing="0" style="border: 2px solid black; width: 500px; height: 200px;">
-            <tr>
-                <th>Title : </th><td>' . $title . '</td>
-            </tr>
-             <tr style="background-color: #e0e0e0;">
-                <th>Description : </th><td>' . $description . '</td>
-            </tr>
-        </table>
-    </body>
-    </html> ', $headers); 
-        if ($mail == true) {
-            $error = "false";
-        } else {
-            $error = "true";
-        }
-        }
+    {      
+      	$connected=self::CheckSMTPstatus();
+	if ($connected===true) {
+	        $file = new File();
+	        $config=$file->ConfigFile();
+	        $headers = "From:<".$config['NO_REPLY_MAIL'].">\r\n";
+	        $headers .= "MIME-Version: 1.0\r\n";
+	        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+	        foreach ($authors as $key => $value) {
+	        $mail = mail($value['MAIL'], 'Dataset submit successfully! ', '<html>
+	    <head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    </head>
+	    <body>
+	        <h2>Your dataset is now on <img src="'.$config['REPOSITORY_URL'].'/img/logo.png" alt="Logo " height="30" width="120" /></h2>
+	        <p> '.$_SESSION['name'].' '.$_SESSION['firstname'].' has published this dataset </p>
+	        <p> Your DOI is : <a href="http://dx.doi.org/' . $doi . '">' . $doi . '</a></p>
+	         <table cellspacing="0" style="border: 2px solid black; width: 500px; height: 200px;">
+	            <tr>
+	                <th>Title : </th><td>' . $title . '</td>
+	            </tr>
+	             <tr style="background-color: #e0e0e0;">
+	                <th>Description : </th><td>' . $description . '</td>
+	            </tr>
+	        </table>
+	    </body>
+	    </html> ', $headers); 
+	        if ($mail == true) {
+	            $error = "false";
+	        } else {
+	            $error = "true";
+	        }
+	        }
+	  }  else{
+  	$error="true";
+  }
         return $error;
     }
 
 
     function DOIerror(){
+    	$connected=self::CheckSMTPstatus();
+	if ($connected===true) {
     	$file = new File();
         $config=$file->ConfigFile();
     	 foreach ($config["admin"] as $key => $value) {
@@ -164,10 +201,13 @@ Class MailerController
                 </body>
                 </html> ', $headers);
             }
-    }
+    	}
+	}
 
 
    function Warning_mail($filename){
+   	$connected=self::CheckSMTPstatus();
+	if ($connected===true) {
         $file = new File();
         $config=$file->ConfigFile();
           foreach ($config["admin"] as $key => $value) {
@@ -183,27 +223,31 @@ Class MailerController
             <p>This file is published, but unpublished data '.$filename.' is not removed, please remove it and create an html file<p>
         </body>
         </html> ', $headers);
-        }
-    }
+        	}
+    	}
+	}
 
      function Warning_mail_bad_path_data(){
-        $file = new File();
-        $config=$file->ConfigFile();
-          foreach ($config["admin"] as $key => $value) {
-                $array = explode(",", $value);
-            }
-            foreach ($array as $key => $value) {
-        $headers = "From:<".$config['NO_REPLY_MAIL'].">\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
-        $mail = mail($value, 'Error in '.$config['REPOSITORY_NAME'], '<html>
-        <body>
-            <h2>Error occured in '.$config['REPOSITORY_NAME'].'!</h2>
-            <p>Your path for file is set to'.$config['UPLOAD_FOLDER'].', and it does not exist or have bad permissions. <p>
-        </body>
-        </html> ', $headers);
-        }
-    }
+     	$connected=self::CheckSMTPstatus();
+	if ($connected===true) {
+	        $file = new File();
+	        $config=$file->ConfigFile();
+	          foreach ($config["admin"] as $key => $value) {
+	                $array = explode(",", $value);
+	            }
+	            foreach ($array as $key => $value) {
+	        $headers = "From:<".$config['NO_REPLY_MAIL'].">\r\n";
+	        $headers .= "MIME-Version: 1.0\r\n";
+	        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+	        $mail = mail($value, 'Error in '.$config['REPOSITORY_NAME'], '<html>
+	        <body>
+	            <h2>Error occured in '.$config['REPOSITORY_NAME'].'!</h2>
+	            <p>Your path for file is set to'.$config['UPLOAD_FOLDER'].', and it does not exist or have bad permissions. <p>
+	        </body>
+	        </html> ', $headers);
+	        }
+	    }
+	}
 
 
 
