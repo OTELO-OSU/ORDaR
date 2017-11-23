9b5aaf375d13ca4a30e55534f30330c738ed5ed8
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-11-23 10:42:39 +0100

    Fix

--- Frontend/InstallAuthDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-$restore_file  = "authentication.sql";
-$config = parse_ini_file('AuthDB.ini');
-$server_name   = $config['host'];
-$username      = $config['username'];
-$password      = $config['password'];
-$cmd = "mysql -h {$server_name} -u {$username} -p{$password}  < $restore_file";
-exec($cmd);
-?>
